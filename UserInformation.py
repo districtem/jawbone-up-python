426d520cad1bc80baeb95f52aaa6e9315e47dfbc
DistrictEm <emily@districtem.com>
2013-10-02 21:30:22 -0400

    adding remaining Userendpoints

--- UserInformation.py
+++ UserInformation.py
@@ -2,13 +2,24 @@ import requests
 
 def get_user_information(username):
 	username = '@' + username
-	user_information = requests.get(https://jawbone.com/nudge/api/users/username)
+	user_information = requests.get('https://jawbone.com/nudge/api/users/username')
 	return user_information
 
-def get_users_friends(username):
+def get_user_friends(username):
 	user = '@' + username
-	users_friends = requests.get(https://jawbone.com/nudge/api/users/username/friends)
-	return users_friends
+	user_friends = requests.get('https://jawbone.com/nudge/api/users/username/friends')
+	return user_friends
 
+def get_user_mood(username):
+	user = '@' + username
+	user_mood = requests.get('https://jawbone.com/nudge/api/users/username/mood')
+	return user_mood
+
+def get_user_trends(username):
+	user = '@' + username
+	user_trends = requests.get('https://jawbone.com/nudge/api/users/username/trend')
+	return user_trends
 
 # next step, create class, stand up class w/ calls
+
+
