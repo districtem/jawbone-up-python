db730bc697f3bd0702a01811a63136d2fd85f657
DistrictEm <emily@districtem.com>
2013-10-03 21:38:54 -0400

    adding json decoding

--- APICalls.py
+++ APICalls.py
@@ -14,61 +14,67 @@ class UserRequests(object):
 
 	def get_user_information(self):
 		user_information = requests.get(self.user_request_url_base)
-		return user_information
+		return user_information.json
 
 	def get_user_friends(self):
 		user_friends = requests.get(self.user_request_url_base + '/friends')
-		return user_friends
+		return user_friends.json
 
 	def get_user_mood(self):
 		user_mood = requests.get(self.user_request_url_base + '/mood')
-		return user_mood
+		return user_mood.json
 
 	def get_user_trends(self):
 		user_trends = requests.get(self.user_request_url_base + '/trend')
-		return user_trends
+		return user_trends.json
 
 # MOVES
 def get_user_moves(user):
 	user = UserRequests(username=user)
 	user_api_url = user.user_request_url_base
 	user_moves = requests.get(user_api_url + '/moves')
-	return user_moves
+	return user_moves.json
 
 def get_specific_move(move_xid):
 	move_id = move_xid
-	return requests.get('https://jawbone.com/nudge/api/v.1.0/moves/{%s}' % move_id)
+	specific_move = requests.get('https://jawbone.com/nudge/api/v.1.0/moves/{%s}' % move_id)
+	return specific_move.json
 
 def get_move_graphs(move_xid):
 	move_id = move_xid
-	return requests.get('https://jawbone.com/nudge/api/v.1.0/moves/{%s}/image' % move_id)
+	move_graphs = requests.get('https://jawbone.com/nudge/api/v.1.0/moves/{%s}/image' % move_id)
+	return move_graphs.json
 
 def get_move_intensity(move_xid):
 	move_id = move_xid
-	return requests.get('https://jawbone.come/nudge/api/v.1.0/moves/{%s}/snapshot')	
-
+	move_intensity = requests.get('https://jawbone.come/nudge/api/v.1.0/moves/{%s}/snapshot')	
+	return move_intensity.json
 
 #  WORKOUTS
 def get_user_workouts(user):
 	user = UserRequests(username=user)
 	user_api_url = user.user_request_url_base
 	user_workouts = requests.get(user_api_url + '/workouts')
-	return user_workouts
+	return user_workouts.json
 
 def get_specific_workout(workout_xid):
 	workout_id = workout_xid
-	return requests.get('https://jawbone.com/nudge/api/v.1.0/workouts/{%s}' % workout_id)
+	specific_workout = requests.get('https://jawbone.com/nudge/api/v.1.0/workouts/{%s}' % workout_id)
+	return specific_workout.json
 
 def get_user_workout_graphs(workout_xid):
 	workout_id = workout_xid
-	return requests.get('https://jawbone.com/nudge/api/v.1.0/workouts/{%s}/image' % workout_id)
+	workout_graphs = requests.get('https://jawbone.com/nudge/api/v.1.0/workouts/{%s}/image' % workout_id)
+	return workout_graphs.json
 
 def get_workout_intensity(workout_xid):
 	workout_id = workout_xid
-	return requests.get('https://jawbone.com/nudge/api/v.1.0/workouts/{%s}/snapshot' % workout_id)
+	workout_intensity = requests.get('https://jawbone.com/nudge/api/v.1.0/workouts/{%s}/snapshot' % workout_id)
+	return workout_intensity.json
+
 
 def create_new_workout():
-	pass
+	
 
 
 
